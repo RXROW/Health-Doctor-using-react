bf6d96bb3efe6ee63922b8a9bf6990b44b09204d
“Eslam“ <aslamalmswdya@gmail.com>
2023-11-06 15:35:35 +0200

    add Landing Page And Somethings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import NavBar from './componats/NavBar';
 import './index.css'
- 
+ import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Home/>
    
     </div>
   );
