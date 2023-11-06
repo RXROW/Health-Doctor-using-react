5ea9eff23dae60d762a0222ba19a0b1b26f8c273
“Eslam“ <aslamalmswdya@gmail.com>
2023-11-06 18:49:29 +0200

    test

--- src/componats/Footer/Footer.js
+++ src/componats/Footer/Footer.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import LogoFooter from '../../assites/logo.png'
 
 const Footer = () => {
-    return (
-       <></>
-    );
-}
+  return (
+    <>
+      <Footer>
+        <Container>
+          <Row>
+            <div className="col-md-3 col-lg-6">
+            <img src={LogoFooter}/>
+            <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page when 
+            looking at its layout. The point of using Lorem Ipsum
+            </p>
+            
+
+            </div>
+            <div className="col-md-8 col-lg-8"></div>
+            <div className="col-md-8 col-lg-8"></div>
+            <div className="col-md-8 col-lg-8"></div>
+          </Row>
+        </Container>
+      </Footer>
+    </>
+  );
+};
 
 export default Footer;
